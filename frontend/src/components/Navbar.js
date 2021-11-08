d5b092adf6cffbd0e787ee6e1850bbeee8f9a873
vishalsinghhh <vishalsingh0710vs@gmail.com>
2021-11-08 11:22:12 +0530

    Hero

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import * as ReactBootStrap from "react-bootstrap";
+import logo from "../images/logo6.png";
+import "./Components.css";
 
-const Navbar = () => {
-    return (
-        <div>
-            navbar
-        </div>
-    )
-}
+const NavbarHome = () => {
+  return (
+    <ReactBootStrap.Navbar expand="lg">
+      <ReactBootStrap.Container>
+        <ReactBootStrap.Navbar.Brand href="#home">
+          <div className="navbarlogo">
+            <img src={logo} alt="logo" height="90" />
+          </div>
+        </ReactBootStrap.Navbar.Brand>
+        <ReactBootStrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <ReactBootStrap.Navbar.Collapse id="basic-navbar-nav">
+          <ReactBootStrap.Nav className="me-auto">
+            <ReactBootStrap.Nav.Link href="#about">
+              <div className="navbar-content">About</div>
+            </ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href="#contact">
+              <div className="navbar-content">Contact</div>
+            </ReactBootStrap.Nav.Link>
+          </ReactBootStrap.Nav>
+        </ReactBootStrap.Navbar.Collapse>
+      </ReactBootStrap.Container>
+    </ReactBootStrap.Navbar>
+  );
+};
 
-export default Navbar
+export default NavbarHome;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import cover from "../images/cover1.jpg"
+import introVid from "../videos/frnds3.mp4"
+import "./pages.css"
 
 const Home = () => {
     return (
         <div>
-            <h1>home</h1>
+            <div class="intro">
+            <div class="bg-video">
+                <video src={introVid} loop muted autoPlay></video>  
+                <img src={cover} alt="" />
+            </div>
+            <div class="intro-head">
+                <div id="intro-head">
+                      Mitra
+                </div>
+            </div>   
+        </div>
         </div>
     )
 }
